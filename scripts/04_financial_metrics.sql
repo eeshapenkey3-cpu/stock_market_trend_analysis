f74d89801b7c557b6723e8225c439886dec1adfe
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-03-10 10:49:20 +0000

    update 04_financial_metrics

--- scripts/04_financial_metrics.sql
+++ scripts/04_financial_metrics.sql
@@ -1,12 +1,18 @@
 USE sp500_analysis;
 
--- Return on Investment: gains/losses from selling a stock in percentile
+-- 1. ROI Analysis (Row-level math)
 /* SELECT ticker, ROUND((((close_price - open_price) / open_price) * 100.0),2) 
 AS ROI_percent
 FROM prices; */
 
--- volatility: 
-SELECT ticker, ROUND(STDV(close_price), 2) AS volatility
-FROM prices
+-- 2. Volatility Analysis (Aggregated math)
+-- This includes both the Dollar swing and the Percentage swing (CV)
+SELECT 
+    ticker, 
+    -- Dollar Volatility 
+    ROUND(STDDEV(close_price), 2) AS volatility_usd,
+    -- Percent Volatility 
+    ROUND((STDDEV(close_price) / AVG(close_price)) * 100, 2) AS volatility_percent
+FROM prices 
 GROUP BY ticker
-ORDER BY volatility DESC;
+ORDER BY volatility_usd DESC, volatility_percent DESC;
