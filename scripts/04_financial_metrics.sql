56eb5b9810d15d52327f830f70f5fa87231b9464
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-03-10 09:57:16 +0000

    update 04_financial_metrics.sql

--- scripts/04_financial_metrics.sql
+++ scripts/04_financial_metrics.sql
@@ -1,6 +1,12 @@
 USE sp500_analysis;
 
 -- Return on Investment: gains/losses from selling a stock in percentile
-SELECT ticker, ROUND((((close_price - open_price) / open_price) * 100.0),2) 
+/* SELECT ticker, ROUND((((close_price - open_price) / open_price) * 100.0),2) 
 AS ROI_percent
-FROM prices;
+FROM prices; */
+
+-- volatility: 
+SELECT ticker, ROUND(STDV(close_price), 2) AS volatility
+FROM prices
+GROUP BY ticker
+ORDER BY volatility DESC;
