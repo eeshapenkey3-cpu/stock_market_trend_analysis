0baebe623ad683ce979199fd8b7511744c06f029
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-03-10 11:22:15 +0000

    altered column name in company_info table

--- scripts/02_metadata_setup.sql
+++ scripts/02_metadata_setup.sql
@@ -1,11 +1,11 @@
 USE sp500_analysis;
 -- reference table
 CREATE TABLE IF NOT EXISTS company_info (
-	symbol VARCHAR(10) PRIMARY KEY,
+	ticker VARCHAR(10) PRIMARY KEY,
     company_name VARCHAR(100),
     sector VARCHAR(50)
 );
-INSERT INTO company_info (symbol, company_name, sector) VALUES
+INSERT INTO company_info (ticker, company_name, sector) VALUES
 ('AAPL', 'Apple Inc.', 'Technology'),
 ('AMZN', 'Amazon.com Inc.', 'Consumer Discretionary'),
 ('GOOGL', 'Alphabet Inc.', 'Communication Services'),
@@ -14,3 +14,6 @@ INSERT INTO company_info (symbol, company_name, sector) VALUES
 ('JPM', 'JPMorgan Chase & Co.', 'Financials'),
 ('V', 'Visa Inc.', 'Financials'),
 ('NVDA', 'NVIDIA Corporation', 'Technology');
+
+/*ALTER TABLE company_info
+RENAME COLUMN symbol TO ticker;*/
--- scripts/04_financial_metrics.sql
+++ scripts/04_financial_metrics.sql
@@ -7,7 +7,7 @@ FROM prices; */
 
 -- 2. Volatility Analysis (Aggregated math)
 -- This includes both the Dollar swing and the Percentage swing (CV)
-SELECT 
+/*SELECT 
     ticker, 
     -- Dollar Volatility 
     ROUND(STDDEV(close_price), 2) AS volatility_usd,
@@ -15,4 +15,14 @@ SELECT
     ROUND((STDDEV(close_price) / AVG(close_price)) * 100, 2) AS volatility_percent
 FROM prices 
 GROUP BY ticker
-ORDER BY volatility_usd DESC, volatility_percent DESC;
+ORDER BY volatility_usd DESC, volatility_percent DESC; */
+
+-- 3.sector analysis
+SELECT c.sector,
+COUNT(p.ticker) AS num_stocks,
+ROUND(AVG(((p.open_price - p.close_price)/p.open_price) * 100.0),2) AS avg_ROI
+FROM prices AS p
+JOIN company_info AS c 
+ON p.ticker = c.ticker
+GROUP BY c.sector
+ORDER BY avg_ROI DESC;
